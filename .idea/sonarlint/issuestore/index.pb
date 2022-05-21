8766a3f9fe239fd896f14c3e8a2aecbc677782fe
Eguonor Umurhurhu <umurhurhu.eguonor@gmail.com>
2022-05-21 16:12:46 +0100

    View pager added to container fragment to hold the signin and signup fragments.

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,20 @@
+
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
+X
+(gradle/wrapper/gradle-wrapper.properties,f\b\fbe448ebfc3eb2d4e308f6b8b043666f5b57235e
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+@
+app/build.gradle,f\4\f4a01d6a4fcb971362ec00a83903fd3902f52164
+P
+ app/src/main/AndroidManifest.xml,8\c\8c55c3ccc257e5907959013f99656e4c8ec3903e
+•
+eapp/src/main/java/com/laurentdarl/quickyfood/presentation/activities/onboarding/OnboardingActivity.kt,d\5\d5e4b4a4bdb449f3939f870f137e2bc1643fac74
+„
+Tapp/src/main/java/com/laurentdarl/quickyfood/data/onboarding/model/OnboardingItem.kt,9\d\9d2cdb1946c2f173691b6be1cbeea6b661a55b66
+Ž
+^app/src/main/java/com/laurentdarl/quickyfood/data/onboarding/adapter/OnboardingItemsAdapter.kt,d\0\d0c86e4c736c1aabc90943e81ee79cbb23abf76c
+‘
+aapp/src/main/java/com/laurentdarl/quickyfood/presentation/activities/splashscreen/SplashScreen.kt,6\b\6b48e8ecb3cc3072024de6b897afcac4f1c406d6
--- app/src/main/java/com/laurentdarl/quickyfood/presentation/activities/mainactivity/MainActivity.kt
+++ app/src/main/java/com/laurentdarl/quickyfood/presentation/activities/mainactivity/MainActivity.kt
@@ -2,22 +2,42 @@ package com.laurentdarl.quickyfood.presentation.activities.mainactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.ktx.Firebase
 import com.laurentdarl.quickyfood.R
 import com.laurentdarl.quickyfood.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+//        setSupportActionBar(binding.mainToolbar)
+
+        // Obtain the FirebaseAnalytics instance.
+        firebaseAnalytics = Firebase.analytics
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf()
+        )
 
         val navHost = supportFragmentManager.findFragmentById(R.id.app_container) as NavHostFragment
         val navController = navHost.navController
-        setupActionBarWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+//        binding.mainToolbar.setupWithNavController(navController, appBarConfiguration)
     }
+
+    override fun onSupportNavigateUp() = Navigation.findNavController(
+        this, R.id.app_container
+    ).navigateUp()
 }
